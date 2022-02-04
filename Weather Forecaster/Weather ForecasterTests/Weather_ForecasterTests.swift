112d974f756a60c41461790a1c92310f1882220e
Kevin E. Rafferty II <kevin@scanwellhealth.com>
2022-02-04 14:26:36 -0800

    [KERII] Initial project setup.

---
+++ Weather Forecaster/Weather ForecasterTests/Weather_ForecasterTests.swift
@@ -0,0 +1,33 @@
+//
+//  Weather_ForecasterTests.swift
+//  Weather ForecasterTests
+//
+//  Created by Kevin E. Rafferty II on 2/4/22.
+//
+
+import XCTest
+@testable import Weather_Forecaster
+
+class Weather_ForecasterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
